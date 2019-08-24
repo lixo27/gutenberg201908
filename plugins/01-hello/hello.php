9b9b14bd45ce5d51c9099b57f0abdc80106f6a4d
Thiago Senna <trsenna@gmail.com>
2019-08-24 12:21:31 -0300

    initial files

---
+++ plugins/01-hello/hello.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Gutenberg | 01-hello
+ * Author:      Thiago Senna
+ * Author URI:  http://thremes.com.br
+ * Description: A gutenberg plugin with a hello block.
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
