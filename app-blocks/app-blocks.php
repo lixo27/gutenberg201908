0cbc0b3dc875d208b4258f9e0e878a5b15738f28
Thiago Senna <trsenna@gmail.com>
2019-09-08 23:10:17 -0300

    initializing app-blocks project

---
+++ app-blocks/app-blocks.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: App Blocks
+ * Author:      Thiago Senna
+ * Author URI:  http://thremes.com.br
+ * Description: The application blocks plugin.
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package   App\Blocks
+ */
+
+define( 'APP_BLOCKS_PLUGIN', true );
+define( 'APP_BLOCKS_PLUGIN_FILE', __FILE__ );
+
+add_action( 'init', function () {
+
+    wp_register_script(
+        'app-blocks',
+        plugins_url( 'build/index.js', APP_BLOCKS_PLUGIN_FILE ),
+        [ 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-editor', 'underscore' ]
+    );
+
+    wp_register_style(
+        'app-blocks-screen',
+        plugins_url( 'build/styles/screen.css', APP_BLOCKS_PLUGIN_FILE )
+    );
+
+    wp_register_style(
+        'app-blocks-editor',
+        plugins_url( 'build/styles/editor.css', APP_BLOCKS_PLUGIN_FILE )
+    );
+
+    register_block_type( 'app-blocks/app', [
+        'style' => 'app-blocks-screen',
+        'editor_style' => 'app-blocks-editor',
+        'editor_script' => 'app-blocks',
+    ] );
+
+} );
